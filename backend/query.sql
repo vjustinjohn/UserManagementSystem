fcecbb88f6c6b3cc01075c5a673ed3dd9b5c3671
vjustinjohn <vjustinjohn@gmail.com>
2019-10-04 18:58:53 +0200

    comment in query to run in single execution

--- backend/query.sql
+++ backend/query.sql
@@ -1,3 +1,4 @@
+--users table
 CREATE TABLE users (
  id serial PRIMARY KEY,
  first VARCHAR(100),
@@ -9,18 +10,22 @@ CREATE TABLE users (
  added TIMESTAMP NOT NULL
 );
 
+--insert one record into users table
 INSERT INTO users (first, last, email, phone, location, hobby, added)
 VALUES('test', 'user', 'test@user.com', '1234567890', 'Liechtenstein', 'Traveling', CURRENT_DATE)
 
+--groups table
 CREATE TABLE groups (
  id serial PRIMARY KEY,
  name VARCHAR(100),
  added TIMESTAMP NOT NULL
 );
 
+--insert one record into groups table
 INSERT INTO groups (name, added)
 VALUES('group1', CURRENT_DATE)
 
+--users_groups
 CREATE TABLE users_groups (
  id serial PRIMARY KEY,
  userid serial,
@@ -28,4 +33,5 @@ CREATE TABLE users_groups (
  added TIMESTAMP NOT NULL
 );
 
+--insert one record into users_groups table
 INSERT INTO users_groups (userid, groupid, added) VALUES (1,1, CURRENT_DATE)
